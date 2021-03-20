8d5eb03a36f0047ef4d2daa5da5018934802457e
davidsinjaya <davidsinjaya@gmail.com>
2021-03-20 12:23:48 +0800

    David | Add seq to seq predict tagging

--- src/CustomTokenizer.py
+++ src/CustomTokenizer.py
@@ -1,6 +1,7 @@
 from tensorflow.python.keras.preprocessing import sequence
 from tensorflow.python.keras.preprocessing import text
 from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Limit on the number of features to K features.
 TOP_K = 20000
@@ -10,6 +11,8 @@ TOP_K = 20000
 # and less than it will be padded
 MAX_SEQUENCE_LENGTH = 30
 
+def pad_sequence_tokens(tokens_onehot):
+    return pad_sequences(tokens_onehot, maxlen=MAX_SEQUENCE_LENGTH, truncating='post', padding='post', value=[0,0,1])
 
 class CustomTokenizer:
     def __init__(self, train_texts):
@@ -25,10 +28,18 @@ class CustomTokenizer:
         self.tokenizer.fit_on_texts(self.train_texts)
 
     def vectorize_input(self, input):
-
         input = self.tokenizer.texts_to_sequences(input)
         # Fix sequence length to max value. Sequences shorter than the length are
         # padded in the beginning and sequences longer are truncated
         # at the beginning.
         output = sequence.pad_sequences(input, maxlen=self.max_length, truncating='post', padding='post')
         return output
+
+    def reverse_tokenized_to_array_text(self, input):
+        reverse_word_map = dict(map(reversed, self.tokenizer.word_index.items()))
+        output = []
+        for tokens in input:
+            output.append([reverse_word_map[token] if token != 0 else None for token in tokens])
+        return output
+
+
--- src/data.py
+++ src/data.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def raw_to_tokens(raw):
     return raw.lower().split(' ')
 
@@ -49,5 +51,54 @@ def tagged_to_poi_street(tagged):
     """
     raw, poi, street = tagged_to_tokens(tagged)
     return ' '.join(poi) + '/' + ' '.join(street)
+
+
+def tagged_tokens_to_onehot(tagged_tokens):
+    """
+    Convert tagged sentence to POI/street one-hot encoding (3D). Format [POI, STREET, OTHER]
+    :param tagged: e.g. [(word0, 'POI'), (word1, 'POI'), (word2, 'OTHER'), (word3, 'STREET'), (word4, 'STREET')]
+    :return tokens_onehot: e.g. [[1,0,0], [1,0,0], [0,0,1], [0,1,0], [0,1,0]]
+    """
+    onehot=[]
+    for pair in tagged_tokens:
+        tag = pair[1] # get the token type
+        if tag == 'POI':
+            res=[1,0,0]
+        elif tag == 'STREET':
+            res=[0,1,0]
+        else:
+            res=[0,0,1] #tag others
+        onehot.append(res)
+    return np.array(onehot)
+
+
+def get_predicted_tokens(tokens_onehot, raw_tokens):
+    """
+   Get the predicted POI/street by mapping onehot encoder to raw tokens.
+   :param tokens_onehot: e.g. [[1,0,0], [1,0,0], [0,0,1], [0,1,0], [0,1,0]]
+   :param raw_tokens: e.g. ['word0', 'word1', word2', 'word3', 'word4']
+   :return POI/street: e.g. [word0 word1/word3 word4]
+   """
+    pred = np.array(tokens_onehot)
+    ix_poi = np.where(pred == 0)[0]
+    ix_street = np.where(pred == 1)[0]
+
+    pred_poi = []
+    pred_street = []
+    res = ''
+    if len(ix_poi) > 0:
+        for ix in ix_poi:
+            if raw_tokens[ix] is not None:  # omit tagging on padding
+                pred_poi.append(raw_tokens[ix])
+        res += ' '.join(pred_poi)
+
+    res += '/'
+    if len(ix_street) > 0:
+        for ix in ix_street:
+            if raw_tokens[ix] is not None:  # omit tagging on padding
+                pred_street.append(raw_tokens[ix])
+        res += ' '.join(pred_street)
+
+    return res
 
 
